bbe7bb03d1afa83746bfd5240d35bffbee0148de
Marko Anic <anicmarko9>
2023-10-07 19:10:01 +0200

    feat: add TypeORM to Next.js app

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from 'next-auth';
 
-export const authOptions: NextAuthOptions = {
-  // callbacks: {
-  //   session({ session, user }) {
-  //     if (session.user) session.user.id = user.id;
-  //     return session;
-  //   },
-  // },
-  // adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      allowDangerousEmailAccountLinking: true,
-      clientId: process.env.GOOGLE_ID || '',
-      clientSecret: process.env.GOOGLE_SECRET || '',
-    }),
-    GithubProvider({
-      allowDangerousEmailAccountLinking: true,
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from '@Auth/auth.config';
 
 export default NextAuth(authOptions);
