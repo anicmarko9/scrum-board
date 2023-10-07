d0a18d3facab30fb69e354698567cf1d2b7e9c6d
Marko Anic <anicmarko9>
2023-10-07 16:19:54 +0200

    feat: add Github and Google OAuth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,17 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx, mdx}'],
   theme: {
     extend: {},
-    colors: {},
+    colors: {
+      primary: '#fedba6',
+      primaryDarker: '#fece85',
+      secondary: '#7f6743',
+      lighterBG: '#f9f3f1',
+      darkerBG: '#cdbfba',
+      accent: '#fdb441',
+      success: '#2ecc71',
+      danger: '#e74c3c',
+      warning: '#f1c40f',
+    },
     animation: {},
     keyframes: {},
   },
