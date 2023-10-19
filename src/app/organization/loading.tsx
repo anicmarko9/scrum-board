533f2ab661cbea688d81e47307acc4cebc3ff966
Marko Anic <anicmarko9>
2023-10-19 16:29:03 +0200

    fix: loading layout on /organizations page

---
+++ src/app/organization/loading.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+export default function Loading(): JSX.Element {
+  return (
+    <AiOutlineLoading3Quarters className='m-auto h-20 w-20 animate-spin text-accent' />
+  );
+}
--- src/components/LoadingImage.tsx
+++ src/components/LoadingImage.tsx
@@ -1,5 +1,7 @@
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
 export default function LoadingImage(): JSX.Element {
   return (
-    <div className='h-12 w-12 animate-spin rounded-full border-b border-dashed border-black'></div>
+    <AiOutlineLoading3Quarters className='h-12 w-12 animate-spin text-accent' />
   );
 }
