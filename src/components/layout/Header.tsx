df3239fdd56b01763309ea5c2ff6c1ff55e7d6b7
trba1810 <trba8l@live.com>
2023-10-20 04:19:16 +0200

    improve header styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Header(): JSX.Element {
   return (
     <header>
-      <nav className='flex items-center justify-between border-gray-200 bg-gray-800 px-4 py-2 lg:px-16'>
+      <nav className='flex items-center justify-between border-gray-200 bg-primaryDarker px-4 py-2 lg:px-16'>
         <figure className='flex items-center'>
           <Image src='/logo-2.png' alt='Logo' width={40} height={40} />
           <figcaption className='ml-3 whitespace-nowrap text-xl font-semibold text-white'>
@@ -12,27 +12,27 @@ export default function Header(): JSX.Element {
           </figcaption>
         </figure>
         <ul className='hidden font-medium lg:flex lg:space-x-8'>
-          <li className='text-gray-400 hover:text-gray-50'>
+          <li className='bg-accent p-3 text-white hover:text-gray-50'>
             <Link href='#'>Home</Link>
           </li>
-          <li className='text-gray-400 hover:text-gray-50'>
+          <li className='bg-accent p-3 text-white hover:text-gray-50'>
             <Link href='#'>Company</Link>
           </li>
-          <li className='text-gray-400 hover:text-gray-50'>
+          <li className='bg-accent p-3 text-white hover:text-gray-50'>
             <Link href='#'>Marketplace</Link>
           </li>
-          <li className='text-gray-400 hover:text-gray-50'>
+          <li className='bg-accent p-3 text-white hover:text-gray-50'>
             <Link href='#'>Features</Link>
           </li>
-          <li className='text-gray-400 hover:text-gray-50'>
+          <li className='bg-accent p-3 text-white hover:text-gray-50'>
             <Link href='#'>Team</Link>
           </li>
-          <li className='text-gray-400 hover:text-gray-50'>
+          <li className='bg-accent p-3 text-white hover:text-gray-50'>
             <Link href='#'>Contact</Link>
           </li>
         </ul>
         <ul className='flex items-center'>
-          <li className='mr-2 rounded-full px-4 py-2 text-sm font-bold text-white hover:bg-gray-400'>
+          <li className='mr-2 rounded-full bg-gradient-to-r from-warning to-success px-4 py-2 text-sm font-bold text-white hover:bg-gray-400'>
             <Link href='/login'>Log in</Link>
           </li>
           <li className='rounded-full bg-gradient-to-r from-blue-500 to-purple-500 px-4 py-2 text-sm font-bold text-white'>
