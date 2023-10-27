da9f403072da9ac5aa3c76614c46a3c6346b1ee1
trba1810 <trba8l@live.com>
2023-10-27 07:25:08 +0200

    added roadmap component and page

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Roadmap from '@/components/Roadmap/Roadmap';
+import Layout from '@/components/layout/Layout';
+
+const page = () => {
+  return (
+    <Layout>
+      <Roadmap />
+    </Layout>
+  );
+};
+
+export default page;
