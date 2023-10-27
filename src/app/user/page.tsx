b84f7143366c8c0a061c66e1a6e662b71d83df2b
trba1810 <trba8l@live.com>
2023-10-27 07:46:50 +0200

    added /user page with form

---
+++ src/app/user/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import UserSettings from '@/components/UserSettings/UserSettings';
+import Layout from '@/components/layout/Layout';
+
+const page = () => {
+  return (
+    <Layout>
+      <UserSettings />
+    </Layout>
+  );
+};
+
+export default page;
